ac27dbc33030def34fc0091954b9a99ddaeb7243
Tim Boudreau <tim@timboudreau.com>
2020-06-15 19:14:34 -0400

    More antlr output line parser fixes, with tests; improve a few module descriptions; update todos

--- antlr-editing-plugins/antlr-project-helpers-maven/src/main/java/org/nemesis/antlr/project/helpers/maven/AntlrOutputProcessorFactory.java
+++ antlr-editing-plugins/antlr-project-helpers-maven/src/main/java/org/nemesis/antlr/project/helpers/maven/AntlrOutputProcessorFactory.java
@@ -67,13 +67,19 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
     static class AntlrLineProcessor implements OutputProcessor {
 
         private static final String[] SEQS = new String[]{"mojo-execute#antlr4:antlr4"};
-        private static final int STATE_READY = 0;
-        private static final int STATE_RECEIVED_LINE = 1;
-        private static final int STATE_ANTICIPATING_EXCEPTION_TYPE = 2;
-        private static final int STATE_IN_STACK_TRACE = 3;
-        private int state = STATE_READY;
+        static final int STATE_READY = 0;
+        static final int STATE_RECEIVED_LINE = 1;
+        static final int STATE_ANTICIPATING_EXCEPTION_TYPE = 2;
+        static final int STATE_IN_STACK_TRACE = 3;
+        int state = STATE_READY;
         private static final Pattern EXCEPTION_PATTERN = Pattern.compile("^org\\.antlr\\.\\S+Exception");
-        private static final Pattern STACK_TRACE_LINE = Pattern.compile("^\\s+at \\S+$");
+//        private static final Pattern STACK_TRACE_LINE = Pattern.compile("^\\s+at\\s\\S+\\s\\(\\w+\\.\\w+:\\d+\\)$");
+        private static final Pattern STACK_TRACE_LINE = Pattern.compile("^\\s+at\\s\\S+\\s\\(\\w+\\.\\w+:\\d+\\)$");
+        private static final Pattern STACK_TRACE_LINE_2 = Pattern.compile("^\\s+at\\s\\S+\\s\\(Native Method\\)$");
+
+        int state() {
+            return state;
+        }
 
         @Override
         public void sequenceStart(String string, OutputVisitor ov) {
@@ -98,56 +104,77 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
             return SEQS;
         }
 
+        private ErrInfo parseForAntlrError(String line, OutputVisitor ov) {
+            ErrInfo info = ErrInfo.parse(line);
+            if (info != null) {
+                if (info.isExceptionMessage) {
+                    state = STATE_ANTICIPATING_EXCEPTION_TYPE;
+                    LOG.log(Level.FINE, "Processed err line to {0}, is exception message; new state 2", info);
+                    ov.skipLine();
+                } else {
+                    LOG.log(Level.FINE, "Processed err line to {0}, attach listener; new state 1", info);
+                    state = STATE_RECEIVED_LINE;
+                    attachToLine(info, ov);
+                }
+
+            }
+            return info;
+        }
+
         @Override
         public void processLine(String line, OutputVisitor ov) {
+            boolean gotExpectedTransition = doProcessLine(line, ov);
+            if (!gotExpectedTransition) {
+                ErrInfo info = parseForAntlrError(line, ov);
+                if (info == null) {
+                    state = STATE_READY;
+                }
+            }
+        }
+
+        private boolean exceptionType(String line, OutputVisitor ov) {
+            Matcher exceptionMatcher = EXCEPTION_PATTERN.matcher(line);
+            if (exceptionMatcher.matches()) {
+                LOG.log(Level.FINEST, "Antipated and matched exception type {0}; to state 3", line);
+                state = STATE_IN_STACK_TRACE;
+                ov.skipLine();
+                return true;
+            }
+            return false;
+        }
+
+        public boolean doProcessLine(String line, OutputVisitor ov) {
+            ErrInfo info = null;
             switch (state) {
                 case STATE_READY:
                 case STATE_RECEIVED_LINE:
-                    ErrInfo info = ErrInfo.parse(line);
-                    if (info != null) {
-                        if (info.isExceptionMessage) {
-                            state = STATE_ANTICIPATING_EXCEPTION_TYPE;
-                            LOG.log(Level.FINE, "Processed err line to {0}, is exception message; new state 2", info);
-//                            ov.skipLine();
-                        } else {
-                            LOG.log(Level.FINE, "Processed err line to {0}, attach listener; new state 1", info);
-                            state = STATE_RECEIVED_LINE;
-                            attachToLine(info, ov);
+                    info = parseForAntlrError(line, ov);
+                    if (info == null) {
+                        if (!exceptionType(line, ov)) {
+                            state = STATE_READY;
+                            LOG.log(Level.FINEST, "Not recognized: {0}", line);
                         }
-                    } else {
-                        state = STATE_READY;
-                        LOG.log(Level.FINEST, "Not recognized: {0}", line);
                     }
-                    break;
+                    return true;
                 case STATE_ANTICIPATING_EXCEPTION_TYPE:
-                    Matcher exceptionMatcher = EXCEPTION_PATTERN.matcher(line);
-                    if (exceptionMatcher.matches()) {
-                        LOG.log(Level.FINEST, "Antipated and matched exception type {0}; to state 3", line);
-                        state = STATE_IN_STACK_TRACE;
-//                        ov.skipLine();
-                    } else {
-                        LOG.log(Level.FINER, "Anticipating exception type, got {0} - to state 0", line);;
-                        state = STATE_READY;
-                    }
-                    break;
+                    return exceptionType(line, ov);
                 case STATE_IN_STACK_TRACE:
                     Matcher traceMatcher = STACK_TRACE_LINE.matcher(line);
+                    if (!traceMatcher.matches()) {
+                        traceMatcher = STACK_TRACE_LINE_2.matcher(line);
+                    }
                     if (traceMatcher.matches()) {
                         if (LOG.isLoggable(Level.FINEST)) {
                             LOG.log(Level.FINEST, "Antipated and matched stack trace line {0}; stay in state 3", line.trim());
                         }
                         ov.skipLine();
+                        return true;
                     } else {
-                        LOG.log(Level.FINER, "Anticipating stack line, got {0} - to state 0", line);;
-                        state = STATE_READY;
+                        return false;
                     }
-                    break;
                 default:
-                    if (state != STATE_READY) {
-                        LOG.log(Level.WARNING, "Unexpected state {0}, to state 0", state);
-                    }
-                    state = STATE_READY;
-                    break;
+                    LOG.log(Level.WARNING, "Unexpected state {0}, to state 0", state);
+                    return false;
             }
         }
 
@@ -158,7 +185,7 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
 
         static class Listener implements OutputListener {
 
-            private final ErrInfo info;
+            final ErrInfo info;
 
             public Listener(ErrInfo info) {
                 this.info = info;
@@ -175,8 +202,13 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
                         EditorCookie cookie = dob.getLookup().lookup(EditorCookie.class);
                         if (cookie != null) {
                             try {
-                                cookie.getLineSet().getCurrent(info.line).show(
-                                        Line.ShowOpenType.OPEN, Line.ShowVisibilityType.FOCUS);
+                                if (info.line >= 0 && info.lineOffset >= 0) {
+                                    cookie.getLineSet().getCurrent(info.line).show(
+                                            Line.ShowOpenType.OPEN, Line.ShowVisibilityType.FOCUS);
+                                } else {
+                                    cookie.getLineSet().getCurrent(0).show(
+                                            Line.ShowOpenType.OPEN, Line.ShowVisibilityType.FOCUS);
+                                }
                             } catch (IndexOutOfBoundsException ex) {
                                 cookie.open();
                             }
@@ -207,11 +239,13 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
             private static final String WINDOWS_FILE_PATTERN = "^(?:[a-zA-Z]\\:|\\\\\\\\[\\w\\.]+\\\\[\\w.$]+)\\\\(?:[\\w]+\\\\)*\\w([\\w.])+";
             private static final String UNIX_FILE_PATTERN = "^(\\/\\S.*?)+\\S.*?\\.g4";
             private static final String FILE_PATTERN = win ? WINDOWS_FILE_PATTERN : UNIX_FILE_PATTERN;
-            private static final String ERR_MATCH_1 = "\\s\\[(\\d+):(\\d+)\\]:\\s(\\w.*)$";
-            private static final String ERR_MATCH_2 = ":(\\d+):(\\d+):\\s(\\w.*)$";
+            private static final String ERR_MATCH_1 = "\\s\\[(\\-?\\d+):(\\-?\\d+)\\]:\\s(\\w.*)$";
+            private static final String ERR_MATCH_2 = ":(\\-?\\d+):(\\-?\\d+):\\s(\\w.*)$";
+            private static final String ERR_MATCH_3 = ":::\\s(\\w.*)$";
 
             private static final Pattern FM1 = Pattern.compile(FILE_PATTERN + ERR_MATCH_1);
             private static final Pattern FM2 = Pattern.compile(FILE_PATTERN + ERR_MATCH_2);
+            private static final Pattern FM3 = Pattern.compile(FILE_PATTERN + ERR_MATCH_3);
 
             public final String file;
             public final int line;
@@ -238,12 +272,28 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
                 // fiddle with the pom file
                 Matcher m = pattern1().matcher(string);
                 boolean isP1 = true;
+                boolean isP3 = false;
                 if (!m.find()) {
                     m = pattern2().matcher(string);
                     if (!m.find()) {
-                        return null;
+                        m = pattern3().matcher(string);
+                        if (!m.find()) {
+                            return null;
+                        }
+                        isP3 = true;
+                        isP1 = false;
+                    } else {
+                        isP1 = false;
                     }
-                    isP1 = false;
+                }
+                if (isP3) {
+                    // error about unexpected EOF tag will have line/position
+                    // -1:-1, and the exception form will omit line position info
+                    // entirely
+                    int msgStart = m.start(2);
+                    String path = string.substring(0, msgStart - 4);
+                    String msg = string.substring(msgStart);
+                    return new ErrInfo(path, 0, 0, false, msg);
                 }
                 int gc = m.groupCount();
                 String positionInLine = m.group(gc - 1);
@@ -251,7 +301,8 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
                 int lineNumberStart = m.start(gc - 2);
                 String path = string.substring(0, lineNumberStart - (isP1 ? 2 : 1));
                 String message = m.group(gc);
-                return new ErrInfo(path, Integer.parseInt(line), Integer.parseInt(positionInLine), isP1, message);
+                // just use start of file in case no line info
+                return new ErrInfo(path, Math.max(0, Integer.parseInt(line)), Math.max(0, Integer.parseInt(positionInLine)), isP1, message);
             }
 
             // For unit tests, allow windows
@@ -280,6 +331,20 @@ public class AntlrOutputProcessorFactory implements OutputProcessorFactory {
                         throw new AssertionError(mode);
                 }
             }
+
+            private static Pattern pattern3() {
+                switch (mode) {
+                    case DEFAULT:
+                        return FM3;
+                    case WINDOWS:
+                        return win ? FM3 : Pattern.compile(WINDOWS_FILE_PATTERN + ERR_MATCH_2);
+                    case UNIX:
+                        return !win ? FM3 : Pattern.compile(UNIX_FILE_PATTERN + ERR_MATCH_3);
+                    default:
+                        throw new AssertionError(mode);
+                }
+            }
+
             private static ParsingMode mode = ParsingMode.DEFAULT;
 
             static void setParsingMode(ParsingMode mode) {
--- antlr-editing-plugins/antlr-project-helpers-maven/src/test/java/org/nemesis/antlr/project/helpers/maven/AntlrOutputProcessorFactoryTest.java
+++ antlr-editing-plugins/antlr-project-helpers-maven/src/test/java/org/nemesis/antlr/project/helpers/maven/AntlrOutputProcessorFactoryTest.java
@@ -15,16 +15,32 @@
  */
 package org.nemesis.antlr.project.helpers.maven;
 
+import com.mastfrog.util.streams.Streams;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.nemesis.antlr.project.helpers.maven.AntlrOutputProcessorFactory.AntlrLineProcessor;
 import org.nemesis.antlr.project.helpers.maven.AntlrOutputProcessorFactory.AntlrLineProcessor.ErrInfo;
+import org.netbeans.api.project.Project;
+import org.netbeans.modules.maven.api.output.OutputProcessor;
+import org.netbeans.modules.maven.api.output.OutputVisitor;
+import org.netbeans.modules.maven.api.output.OutputVisitor.Context;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
 
 /**
  *
  * @author Tim Boudreau
  */
+@Execution(ExecutionMode.SAME_THREAD)
 public class AntlrOutputProcessorFactoryTest {
 
     private static final String ERR_1 = "/home/tim/work/personal/imagine/Imagine/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4:130:0: error: syntax error: '}' came as a complete surprise to me [error 50]";
@@ -34,13 +50,14 @@ public class AntlrOutputProcessorFactoryTest {
     private static final String ERR_5 = "/home/tim/work/personal/imagine/Imagine/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4:130:44: error: reference to parser rule gwerb in lexer rule gwerb [error 160]";
     private static final String ERR_6 = "/home/tim/work/personal/imagine/Imagine/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4 [130:5555]: reference to parser rule gwerb in lexer rule gwerb";
     private static final String ERR_7 = "/home/folder\\ with\\ spaces/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4 [130:66]: reference to parser rule gwerb in lexer rule gwerb";
+    private static final String ERR_8 = "/home/tim/work/personal/personal/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4::: error: syntax error: mismatched character '<EOF>' expecting '\"' [error 50]";
 
     private static final String W_ERR_1 = "C:\\foo\\bar\\MarkdownLexer.md:130:77: error: syntax error: '}' came as a complete surprise to me [error 50]";
     private static final String W_ERR_2 = "\\\\Server\\foo\\bar\\MarkdownLexer.md:130:8888: error: syntax error: '}' came as a complete surprise to me [error 50]";
 
     @Test
     public void testParseErrors() {
-        for (String err : new String[]{ERR_1, ERR_2, ERR_3, ERR_4, ERR_5, ERR_6, ERR_7, W_ERR_1, W_ERR_2}) {
+        for (String err : new String[]{ERR_1, ERR_2, ERR_3, ERR_4, ERR_5, ERR_6, ERR_7, ERR_8, W_ERR_1, W_ERR_2}) {
             testOne(err);
         }
     }
@@ -61,12 +78,12 @@ public class AntlrOutputProcessorFactoryTest {
         int expPos;
         boolean expClosingBracket = false;
         String expFile = "/home/tim/work/personal/imagine/Imagine/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4";
-        switch(err) {
-            case ERR_1 :
+        switch (err) {
+            case ERR_1:
                 expClosingBracket = true;
                 expPos = 0;
                 break;
-            case ERR_2 :
+            case ERR_2:
                 expPos = 11;
                 break;
             case ERR_3:
@@ -87,23 +104,229 @@ public class AntlrOutputProcessorFactoryTest {
                 expPos = 66;
                 expFile = "/home/folder\\ with\\ spaces/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4";
                 break;
-            case W_ERR_1 :
+            case ERR_8 :
+                expPos = 0;
+                expFile = "/home/tim/work/personal/personal/markdown-grammar/src/main/antlr4/org/imagine/markdown/grammar/MarkdownLexer.g4";
+                expClosingBracket = true;
+                assertEquals(0, info.line, "-1 offset should have been converted to 0");
+                assertEquals(0, info.lineOffset, "-1 offset should have been converted to 0");
+                break;
+            case W_ERR_1:
                 expFile = "C:\\foo\\bar\\MarkdownLexer.md";
                 expPos = 77;
                 expClosingBracket = true;
                 break;
-            case W_ERR_2 :
+            case W_ERR_2:
                 expPos = 8888;
                 expFile = "\\\\Server\\foo\\bar\\MarkdownLexer.md";
                 expClosingBracket = true;
                 break;
-            default :
+            default:
                 throw new AssertionError("Unknown text: " + err);
         }
         assertEquals(expPos, info.lineOffset);
         assertEquals(expFile, info.file);
-        assertEquals(expClosingBracket, info.message.charAt(info.message.length()-1) == ']');
+        assertEquals(expClosingBracket, info.message.charAt(info.message.length() - 1) == ']');
         assertEquals(expClosingBracket, info.isExceptionMessage);
     }
 
+    @Test
+    public void testMavenSessionPlayback() throws IOException {
+        // We use a microformat that lets us embed assertions at the head of lines about what
+        //
+        ErrInfo.setParsingMode(ErrInfo.ParsingMode.UNIX);
+        String session;
+        try (InputStream in = AntlrOutputProcessorFactoryTest.class.getResourceAsStream("errors-session.txt")) {
+            assertNotNull(in, "errors-session.txt is not adjacent to AntlrOutputProcessorFactoryTest");
+            session = Streams.readString(in);
+        }
+        AntlrOutputProcessorFactory fact = new AntlrOutputProcessorFactory();
+        Set<? extends OutputProcessor> set = fact.createProcessorsSet(new FakeProject());
+        assertNotNull(set);
+        assertFalse(set.isEmpty());
+        assertEquals(1, set.size());
+        OutputProcessor op = set.iterator().next();
+        assertTrue(op instanceof AntlrOutputProcessorFactory.AntlrLineProcessor);
+        String[] lines = session.split("\n");
+        AntlrLineProcessor lp = (AntlrLineProcessor) op;
+        OutputVisitor ov = new OutputVisitor(new Context() {
+            @Override
+            public Project getCurrentProject() {
+                return new FakeProject();
+            }
+        });
+        lp.sequenceStart("mojo-execute#antlr4:antlr4", ov);
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            testLine(line, lp, i + 1);
+        }
+        lp.sequenceFail("mojo-execute#antlr4:antlr4", ov);
+        lp.sequenceEnd("project-execute", ov);
+        lp.sequenceEnd("session-execute", ov);
+    }
+
+    private void testLine(String line, AntlrLineProcessor proc, int lineNumber) {
+        if (line.length() > 0 && line.charAt(0) == ':') {
+            String[] instructionsAndText = line.substring(1).split("\\|\\!\\|");
+            assertTrue(instructionsAndText.length == 2, "Corrupted input line: '"
+                    + line + "' could not split on '|!|' "
+                    + "delimiter but does start with a ':'");
+            handleOneLine(instructionsAndText[0], instructionsAndText[1], proc, new OutputVisitor(), lineNumber);
+        }
+    }
+
+    private void handleOneLine(String instructions, String line, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+        int oldState = proc.state();
+        proc.processLine(line, vis);
+        int newState = proc.state();
+        String[] assertions = instructions.split(";");
+        for (int i = 0; i < assertions.length; i++) {
+            handleOneAssertion(assertions[i], line, oldState, newState, proc, vis, lineNumber);
+        }
+    }
+
+    private void handleOneAssertion(String assertion, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+        boolean handled = false;
+        for (int i = 0; i < allAssertionTypes.length; i++) {
+            handled = allAssertionTypes[i].maybeProcess(assertion, line, oldState, newState, proc, vis, lineNumber);
+            if (handled) {
+                break;
+            }
+        }
+        assertTrue(handled, "Malformed asssertion '" + assertion + "' not recognized at line " + lineNumber);
+    }
+
+    static abstract class AssertionType {
+
+        private final String name;
+
+        public AssertionType(String name) {
+            this.name = name;
+        }
+
+        final boolean maybeProcess(String assertion, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            if (assertion.startsWith(name)) {
+                String input = assertion.substring(name.length()).trim();
+                run(input, line, oldState, newState, proc, vis, lineNumber);
+                return true;
+            }
+            return false;
+        }
+
+        abstract void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber);
+    }
+
+    private static class StateChange extends AssertionType {
+
+        StateChange() {
+            super("StateChange");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            String[] args = tail.split("\\s+");
+            assertEquals(2, args.length, "line " + line + " '" + tail + "' is badly formatted - need two numeric arguments");
+            int from = Integer.parseInt(args[0]);
+            int to = Integer.parseInt(args[1]);
+            assertEquals(oldState, from, "Previous state should be " + from + " but was " + oldState + "at " + lineNumber + ": '" + line + "' states " + oldState + "->" + newState + " expected " + from + "->" + to);
+            assertEquals(newState, to, "Next state should be " + to + " but was " + newState + " at line " + lineNumber+ ": '" + line + "' states " + oldState + "->" + newState + " expected " + from + "->" + to);
+        }
+    }
+
+    private static class LineHidden extends AssertionType {
+
+        LineHidden() {
+            super("LineHidden");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            assertTrue(vis.isLineSkipped(), "Line " + lineNumber + " should have told the OutputVisitor to skip line '" + line + "'");
+        }
+    }
+
+    private static class ErrorListenerSet extends AssertionType {
+
+        ErrorListenerSet() {
+            super("ErrorListenerSet");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            assertNotNull(vis.getOutputListener(), "Listener should have been set for line " + lineNumber + ": '" + line + "'");
+            assertTrue(vis.getOutputListener() instanceof AntlrOutputProcessorFactory.AntlrLineProcessor.Listener);
+            assertNotNull(((AntlrOutputProcessorFactory.AntlrLineProcessor.Listener) vis.getOutputListener()).info);
+        }
+    }
+
+    private static class IsExceptionMessage extends AssertionType {
+
+        IsExceptionMessage() {
+            super("IsExceptionMessage");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            ErrInfo info = ((AntlrOutputProcessorFactory.AntlrLineProcessor.Listener) vis.getOutputListener()).info;
+            assertNotNull(info, "No info set for line " + lineNumber + ": '" + line + "'");
+            assertTrue(info.isExceptionMessage, "Line " + lineNumber + " should have been detected as an exception message, not error output: '" + line + "'");
+        }
+    }
+
+    private static class NotExceptionMessage extends AssertionType {
+
+        NotExceptionMessage() {
+            super("NotExceptionMessage");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            ErrInfo info = ((AntlrOutputProcessorFactory.AntlrLineProcessor.Listener) vis.getOutputListener()).info;
+            assertNotNull(info, "No info set for line " + lineNumber + ": '" + line + "'");
+            assertFalse(info.isExceptionMessage, "Line " + lineNumber + " should have been detected as a error output, not an exception message: '" + line + "'");
+        }
+    }
+
+    private static class StackTraceLine extends AssertionType {
+
+        StackTraceLine() {
+            super("StackTraceLine");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            assertEquals(AntlrOutputProcessorFactory.AntlrLineProcessor.STATE_IN_STACK_TRACE, newState, "Expected state STATE_IN_STACK_TRACE (" + AntlrOutputProcessorFactory.AntlrLineProcessor.STATE_IN_STACK_TRACE + ") for line " + lineNumber + ": '" + line + "'");
+        }
+    }
+
+    private static class Ready extends AssertionType {
+
+        Ready() {
+            super("Ready");
+        }
+
+        @Override
+        void run(String tail, String line, int oldState, int newState, AntlrLineProcessor proc, OutputVisitor vis, int lineNumber) {
+            assertEquals(AntlrOutputProcessorFactory.AntlrLineProcessor.STATE_READY, newState, "Expected state READY at line " + lineNumber + ": '" + line + "'");
+        }
+    }
+
+    private AssertionType[] allAssertionTypes = new AssertionType[]{new StateChange(),
+        new LineHidden(), new ErrorListenerSet(), new IsExceptionMessage(),
+        new NotExceptionMessage(), new StackTraceLine(), new Ready()};
+
+    static final class FakeProject implements Project {
+
+        private final HideAntlrSourceDirsFromMavenOtherSources marker = new HideAntlrSourceDirsFromMavenOtherSources(Lookup.EMPTY);
+
+        @Override
+        public FileObject getProjectDirectory() {
+            return null;
+        }
+
+        @Override
+        public Lookup getLookup() {
+            return Lookups.singleton(marker);
+        }
+    }
 }
